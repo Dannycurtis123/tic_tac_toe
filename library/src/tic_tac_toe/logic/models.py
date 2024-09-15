f993f2db3c3dadbe722970be90a613ab4d7f7542
danny.curtis123 <danny.curtis123@yahoo.com>
2024-09-15 11:08:30 -0500

    counts

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -4,8 +4,12 @@
 
 #region IMPORTS
 from __future__ import annotations
+
 import enum
+import re
+
 from dataclasses import dataclass
+from functools import cached_property
 #endregion IMPORTS
 
 #region PLAYERMARK CLASS
@@ -26,5 +30,36 @@ class PlayerMark(enum.StrEnum):
 #endregion PLAYERMARK CLASS
 
 #region GRID
+@dataclass(frozen = True)
+class NineByNineGrid:
+    '''
+        Define gamimg surface for a 9x9 grid
+        Use frozen=True to lock the class in place
+    '''
+    cells: str = " " * 9
 
+    def __post_init__(self) -> None:
+        if not re.match(r"^[\sXO]{9}$", self.cells):
+            raise ValueError("9x9 Grid can only contain 9 cells of: X, O, or space characters.")
+
+    @cached_property
+    def x_count(self) -> int:
+        '''
+            maintains current count of EXES
+        '''
+        return self.cells.count("X")
+
+    @cached_property
+    def o_count(self) -> int:
+        '''
+            maintains current count of OHS
+        '''
+        return self.cells.count("O")
+
+    @cached_property
+    def empty_count(self) -> int:
+        '''
+            maintains current count of Empty spaces
+        '''
+        return self.cells.count(" ")
 #endregionGRID
